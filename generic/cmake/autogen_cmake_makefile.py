336f34ffcf94288e8130bc9a79471625a8edfde6
mactul <mactulgames@gmail.com>
2025-08-23 20:04:52 +0200

    generic autogen + cmalke

---
+++ generic/cmake/autogen_cmake_makefile.py
@@ -0,0 +1,17 @@
+import powermake
+import typing as T
+
+def on_build(config: powermake.Config):
+    if powermake.run_command(config, ["bash", "autogen.sh"], cwd="..") != 0:
+        raise powermake.PowerMakeRuntimeError("autogen failed")
+    powermake.run_cmake(config, "..", "-DCMAKE_BUILD_TYPE=Release")
+    if powermake.run_command(config, ["make", "-j8"]) != 0:
+        raise powermake.PowerMakeRuntimeError("make failed")
+
+def on_install(config: powermake.Config, install_path: T.Union[str, None]):
+    if install_path is None:
+        install_path = "./install"
+    if powermake.run_command(config, ["cmake", "--install", ".", "--prefix", install_path]) != 0:
+        raise powermake.PowerMakeRuntimeError("cmake install failed")
+
+powermake.run("generic_cmake", build_callback=on_build, install_callback=on_install)

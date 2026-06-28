afdee83de79ff6be13aeaf0c42409ae2045171d6
mactul <mactulgames@gmail.com>
2026-06-28 11:11:53 +0200

    Add a way to recursively install dependencies

--- generic/cmake/cmake_makefile.py
+++ generic/cmake/cmake_makefile.py
@@ -1,26 +1,46 @@
+import os
 import powermake
 import typing as T
+import powermake.package
 
 
 parser = powermake.ArgumentParser()
-parser.add_argument("--cmake-static", help="", action="store_true")
+parser.add_argument("--cmake-static", help="instruct cmake to prefer building a static lib if possible", action="store_true")
+parser.add_argument("--dependency", metavar="DEPENDENCY", help="syntax: libname,min_ver,max_ver, may be given multiple time", action="append", default=[])
+parser.add_argument("--cmake-flag", metavar="FLAG", help="A flag to transmit to CMake, may be given multiple time", action="append", default=[])
 
 args_parsed = parser.parse_args()
 
 
 def on_build(config: powermake.Config):
-    if config._args_parsed.install is not None:
-        install_path = config._args_parsed.install
-    else:
-        install_path = "./install"
+    if config._args_parsed.install is None:
+        raise powermake.PowerMakeValueError("You should use this makefile with -r and --install")
 
-    powermake.run_cmake(config, "..", "-DCMAKE_BUILD_TYPE=Release", f"-DCMAKE_INSTALL_PREFIX={install_path}", prefer_static=args_parsed.cmake_static)
+    install_path = config._args_parsed.install
+
+    dependencies = []
+
+    powermake_libs_dir = os.path.abspath(os.path.join(install_path, "../../../../../"))
+
+
+    for dep in args_parsed.dependency:
+        if dep.count(',') != 2:
+            raise powermake.PowerMakeValueError("dependency syntax: libname,min_ver,max_ver")
+        dep_name, dep_min_ver, dep_max_ver = dep.split(',')
+        if dep_min_ver == 'None':
+            dep_min_ver = None
+        if dep_max_ver == 'None':
+            dep_max_ver = None
+
+        dependencies.append(powermake.package.find_lib(config, dep_name, powermake_libs_dir, min_version=dep_min_ver, max_version=dep_max_ver))
+
+    powermake.run_cmake(config, "..", "-DCMAKE_BUILD_TYPE=Release", f"-DCMAKE_INSTALL_PREFIX={install_path}", *args_parsed.cmake_flag, prefer_static=args_parsed.cmake_static, dependencies=dependencies)
     if powermake.run_command(config, ["make", "-j8"]) != 0:
         raise powermake.PowerMakeRuntimeError("make failed")
 
 def on_install(config: powermake.Config, install_path: T.Union[str, None]):
-    if install_path is None:
-        install_path = "./install"
+    if install_path is None or not config.rebuild:
+        raise powermake.PowerMakeValueError("You should use this makefile with -r and --install")
     if powermake.run_command(config, ["cmake", "--install", ".", "--prefix", install_path]) != 0:
         raise powermake.PowerMakeRuntimeError("cmake install failed")
 
